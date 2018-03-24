a1b340a87c39ac79b6ee0e45d6d6768534cf8cdb
Radu Lăncrănjan <radu.lancra@gmail.com>
2018-03-24 12:29:32 +0200

    Added logging.

--- pc/hscCtrl/Program.cs
+++ pc/hscCtrl/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace hscCtrl
+﻿using Serilog;
+
+namespace hscCtrl
 {
     static class Program
     {
         static void Main(string[] args)
         {
+            SetupLogging();
+        }
+
+        private static void SetupLogging()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
         }
     }
 }
